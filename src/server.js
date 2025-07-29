6603f141b8a05b2c153d76b0baa75d18408e8000
silveriale <leticia.silveria.costa@gmail.com>
2025-07-29 14:58:53 -0300

    Compilando TypeScript
    - npx tsc src/server.ts p converser o arquivo ts p js
    - o TypeScript é recomendado para desenvolvimento, sendo o JavaScript utilizado em produção.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
-console.log("hello word")
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 3);
+console.log("Resultado da soma:", result);
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+const result = sum(5, 3);
+console.log("Resultado da soma:", result);
