5ae1c72959d24c26b9ccaf212e59c5558ba1e862
silveriale <leticia.silveria.costa@gmail.com>
2025-07-30 12:29:05 -0300

    Route Params
    -  dá p passar parâmetros na rota, como o ID do produto, e recuperá-los usando request.params
    - Utiliza-se dois pontos seguidos do nome do parâmetro na rota.
    - É possível passar e recuperar mais de um parâmetro na rota, sem a necessidade de usar os dois pontos ao recuperar.
    -  O Express facilita a extração de informações da rota, tornando o processo mais simples do que utilizando Node Puro.

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ const PORT = 3333; // Define o numero da porta
 
 const app = express(); // Cria uma instância do Express
 
-app.get("/", (request, response) => {
-  response.send("Hello World Express!"); // Define uma rota que responde com "Hello World!"
+app.get("/products/:id/:user", (request, response) => {
+  const { id, user } = request.params; // Extrai o parâmetro 'id' da URL e 'user'
+  response.send(`Produto ${id} do usuário ${user}`); // Envia uma resposta com o ID e User do produto
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // Inicia o servidor e exibe uma mensagem no console
