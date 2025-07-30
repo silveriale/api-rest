ca450e4f9dd94da9f984169849afb921b6205773
silveriale <leticia.silveria.costa@gmail.com>
2025-07-30 12:06:21 -0300

    Enviando requisição com o método GET
    - cria a primeira rota e faz a primeira requisição utilizando o Express.
    - criação de rotas e o envio de respostas utilizando o método send

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const PORT = 3333; // Define o numero da porta
 
 const app = express(); // Cria uma instância do Express
 
+app.get("/", (request, response) => {
+  response.send("Hello World Express!"); // Define uma rota que responde com "Hello World!"
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // Inicia o servidor e exibe uma mensagem no console
