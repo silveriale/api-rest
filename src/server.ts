c2e32558449f427528bfb11206dbb9de832432a8
silveriale <leticia.silveria.costa@gmail.com>
2025-07-31 11:27:36 -0300

    Dados no corpo da requisição
    - Envia dados via POST no Express usando o corpo da requisição (BODY)

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,16 @@ import express from "express";
 const PORT = 3333; // Define o numero da porta
 
 const app = express(); // Cria uma instância do Express
+app.use(express.json()); // utiliza o JSON como estratégia p representar dados
 
 app.get("/products", (request, response) => {
   const { page, limit } = request.query; // Extrai os parâmetros de consulta 'page' e 'limit'. /products?page=1&limit=14
   response.send(`Página ${page} de ${limit}`); // Responde com uma mensagem formatada
 });
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body; // Extrai os parâmetros 'name' e 'price' do corpo da requisição
+  response.send(`Produto ${name} custa R$${price}`); // Responde com uma mensagem de sucesso
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // Inicia o servidor e exibe uma mensagem no console
