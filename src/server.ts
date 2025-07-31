6c32e88a9b42c3889dff3267910057e311210c1a
silveriale <leticia.silveria.costa@gmail.com>
2025-07-31 17:30:27 -0300

    Middleware Local
    - como aplicar um middleware de forma específica em uma rota
    - como inserir o middleware antes de executar uma função em uma rota específica, mostrando a diferença entre um middleware global e um local
    - flexibilidade de escolher entre aplicar o middleware em todas as rotas, em uma rota específica ou em várias rotas de uma vez

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,16 @@ const PORT = 3333; // Define o numero da porta
 const app = express(); // Cria uma instância do Express
 app.use(express.json()); // utiliza o JSON como estratégia p representar dados
 
-app.use(myMiddleware); // Usa o middleware personalizado em todas as rotas (globalmente)
+// Middleware global
+//app.use(myMiddleware); // Usa o middleware personalizado em todas as rotas (globalmente)
 
 app.get("/products", (request, response) => {
   const { page, limit } = request.query; // Extrai os parâmetros de consulta 'page' e 'limit'. /products?page=1&limit=14
   response.send(`Página ${page} de ${limit}`); // Responde com uma mensagem formatada
 });
 
-app.post("/products", (request, response) => {
+app.post("/products", myMiddleware, (request, response) => {
+  // Define uma rota POST para '/products' e usa o middleware de forma local, em uma rota específica
   const { name, price } = request.body; // Extrai os parâmetros 'name' e 'price' do corpo da requisição
   // response.send(`Produto ${name} custa R$${price}`); // Responde com uma mensagem de sucesso
   response.status(201).json({ name, price }); // Responde com um JSON e status 201 (Criado)
